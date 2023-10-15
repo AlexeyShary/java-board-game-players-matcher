5e5b0a16b734e63733b4a3fbc5e6d28a7f8ff1f6
Alexey Shary <badirbis@yandex.ru>
2023-10-15 17:57:17 +0300

    Initial commit

---
+++ src/main/java/sh/alex/boardgameplayersmatcher/BoardGamePlayersMatcherApplication.java
@@ -0,0 +1,13 @@
+package sh.alex.boardgameplayersmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGamePlayersMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGamePlayersMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/alex/boardgameplayersmatcher/BoardGamePlayersMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.alex.boardgameplayersmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGamePlayersMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
